4bdcd5ece78a7abcd2a0caff3ccba7f390e4f0fe
Sebastian <ota.sebas@gmail.com>
2023-01-12 10:31:53 -0500

    commit tree a94a12eb3538402401bf90ad16b1fe1a5dd47f47

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name 
+      t.boolean :is_graduated
+      t.integer :age
+      t.date :graduation_year
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,11 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_graduated"
+    t.integer "age"
+    t.date "graduation_year"
+  end
+
 end
